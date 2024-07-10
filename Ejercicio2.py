f4eb00a334fde34cff62d7fee8096def39978bbc
luisfchuquilla <luisf.chuquilla@ute.edu.ec>
2024-07-09 19:25:51 -0500

    Ejercicios hechos en phyton

---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+#2.-Suma de dos números: Escribe un programa que pida al usuario dos números y los sume.
+numero1 = int(input("Ingrese el primer número: ")) #Pedimos los dos numero al usuario mediante input y convertirlos en enteros (int) para sumarlos
+numero2 = int(input("Ingrese el segundo número: "))
+suma = numero1 + numero2 #Sumamos los dos numeros que pedimos al usuario
+print("El resultado es: ", suma) #Muestra el resultado de la operacion
+     
